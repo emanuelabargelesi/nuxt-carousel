25ed7da4bb35a497e42d502d604bd0a3a74b4ded
Emanuela <ebargelesi@gmail.com>
2021-07-22 19:51:20 +0200

    refactor!: added sorting by river length, remove arrows and css for UI style

--- components/Slider.vue
+++ components/Slider.vue
@@ -1,7 +1,7 @@
 <template>
   <transition name="fade">
     <div class="carousel-slider w-screen mx-auto" v-if="show" :style="style">
-      <VueSlickCarousel :arrows="true" :dots="true" v-bind="settings">
+      <VueSlickCarousel v-bind="settings">
         <template v-for="post in posts">
           <div @mouseover="isHovering = true" @mouseout="isHovering = false">
             <img class="mx-auto rounded-3xl object-fill" :src="post.image" :alt="post.content"
@@ -9,7 +9,7 @@
             <div
               class="card-slider opacity-0 bg-transparent absolute inset-0 flex flex-col justify-center h-full mx-auto md:p-10 sm:p-5 md:text-base sm:text-xs text-white"
               :class="{'opacity-100': isHovering}">
-              <p class="text-2xl" v-text="post.title + ' - ' + post.continent"></p>
+              <p class="text-2xl" v-text="post.title + ' - ' + post.continent"></p> <span v-text="post.length"></span>
               <p class="mt-3" v-text="post.description"></p>
             </div>
           </div>
@@ -35,7 +35,7 @@ export default {
       riverLength: "",
       settings: {
         "dots": true,
-        "arrows": true,
+        "arrows": false,
         "initialSlide": 0,
         "autoplay": false,
         "adaptiveHeight": false,
@@ -44,7 +44,7 @@ export default {
         "fade": true
       },
       isHovering: false,
-      arrows: "#000000",
+      dotsContainer: "#000000",
       dots: "#d3d8f2"
     }
   },
@@ -61,22 +61,22 @@ export default {
 
   methods: {
     changeDark() {
-      this.arrows = "#d3d8f2";
+      this.dotsContainer = "#d3d8f2";
       this.dots = "#1f2a37";
     },
     changeLight() {
-      this.arrows = "#000000";
+      this.dotsContainer = "#000000";
       this.dots = "#d3d8f2";
     }
   },
 
   computed: {
     posts() {
-      return this.response.filter(post => !post.length.indexOf(this.riverLength))
+      return this.response.sort((a, b) => parseFloat(b.length) - parseFloat(a.length))
     },
     style() {
       return {
-        "--arrows": this.arrows,
+        "--dotsContainer": this.dotsContainer,
         "--dots": this.dots
       }
     }
@@ -108,41 +108,8 @@ export default {
   }
 }
 
-.slick-prev:before,
-.slick-next:before {
-  color: var(--arrows);
-}
-
-.slick-next {
+.slick-next, .slick-prev {
   display: none !important;
-  @media only screen and (min-width: 600px) {
-    display: block !important;
-    right: 1rem;
-  }
-  @media only screen and (min-width: 1024px) {
-    display: block !important;
-    right: 10rem;
-  }
-  @media only screen and (min-width: 1400px) {
-    display: block !important;
-    right: 17rem;
-  }
-}
-
-.slick-prev {
-  display: none !important;
-  @media only screen and (min-width: 600px) {
-    display: block !important;
-    left: 1rem;
-  }
-  @media only screen and (min-width: 1024px) {
-    display: block !important;
-    left: 10rem;
-  }
-  @media only screen and (min-width: 1400px) {
-    display: block !important;
-    left: 17rem;
-  }
 }
 
 .fade-enter-active,
@@ -156,7 +123,7 @@ export default {
 }
 
 .slick-dots:not(#_) {
-  background-color: var(--arrows);
+  background-color: var(--dotsContainer);
   margin-top: 2rem;
   display: flex !important;
   left: 50%;
